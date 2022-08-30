9fac3375df2c23cccbfdf30119464afaaecbb5da
Casey Davis <caseydavis100@gmail.com>
2022-08-30 13:02:28 -0600

    link adjustments

--- frontend_react/src/components/NavBar/NavBar.jsx
+++ frontend_react/src/components/NavBar/NavBar.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
 import { images } from '../../constants';
 import './NavBar.scss';
 
 const NavBar = () => {
+  const [toggle, setToggle] = useState(false)
+
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
@@ -17,8 +21,31 @@ const NavBar = () => {
           </li>
         ))}
       </ul>
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)}/>
+
+        {toggle && (
+            <motion.div
+              whileInView={{ x: [300,0] }}
+              transition={{ duration: 0.85, ease: 'easeout'}}
+
+            >
+              <HiX onClick={() => setToggle(false)} />
+              <li key={ item }>
+                <div />
+                <a href={`#${item}`onClick={() => setToggle(false)}}>{item}</a>
+              </li>
+            </motion.div>
+          )
+
+        }
+      </div>
+
     </nav>
   )
 }
 
 export default NavBar
+
+
+Resume video at https://youtu.be/3HNyXCPDQ7Q?t=3187
