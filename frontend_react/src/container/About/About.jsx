327efabb8ca224066b27f819cbe073149f321bb1
Casey Davis <c-casey.davis@charter.com>
2022-09-30 08:59:07 -0600

    fixed minor typos

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -25,12 +25,13 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgurl} alt={about.title} />
+            <img src={about.imgUrl} alt={about.title} />
             <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
             <p className='p-text' style={{ marginTop: 10 }}>{about.Description}</p>
           </motion.div>       
         ))}
     </div>
+    </>
   )
 }
 
