ce31f14863bce0efbbe0b3f3df06b2d4c631ab6e
Casey Davis <caseydavis100@gmail.com>
2022-10-11 19:15:07 -0600

    Updated About section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -3,15 +3,18 @@ import { motion } from 'framer-motion';
 
 import { images } from '../../constants'
 import './About.scss';
-
-const abouts = [
-  {title: 'Web Development', Description: 'I am a good web developer.', imgUrl: images.about01 },
-  {title: 'Web Design', Description: 'I am a good web developer.', imgUrl: images.about02},
-  {title: 'UI/UX', Description: 'I am a good web developer.', imgUrl: images.about03},
-  {title: 'Animation', Description: 'I am a good web developer.', imgUrl: images.about04}
-];
+import { urlFor, client } from '../../client';
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]'
+
+    client.fetch(query)
+      .then((data) => setAbouts(data))
+  }, []);
+  
   return (
     <>
     <h2 className='head-text'> I know that <span> Good Dev </span> <br /> means <span>Good business</span> </h2>
@@ -25,7 +28,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
             <p className='p-text' style={{ marginTop: 10 }}>{about.Description}</p>
           </motion.div>       
